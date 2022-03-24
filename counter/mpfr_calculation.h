81772d00036cfa3a54e76e164c024fdcc89faa4a
zhixian001 <okyjh12@naver.com>
2022-03-24 23:05:17 +0900

    update mpfr calculation code comments

--- counter/mpfr_calculation.h
+++ counter/mpfr_calculation.h
@@ -3,4 +3,12 @@
 #include "mpfr.h"
 #include "gmp.h"
 
+// MPFR Guide: https://www.mpfr.org/mpfr-current/mpfr.html#index-mpfr_005frnd_005ft
+ 
+/**
+ * @brief Calculate the natural logarithm of an (big) integer
+ * 
+ * @param input_int arbitrarily (big) integer expressed as a string
+ * @return double
+ */
 double log_e(char* input_int);
